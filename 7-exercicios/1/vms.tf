091f67e5441b12d2910b7d826823c4f5e187e7d2
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2022-04-11 06:50:58 -0300

    corrigindo versão da aula

--- 7-exercicios/1/vms.tf
+++ 7-exercicios/1/vms.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "vm" {
   for_each      = var.nomes
   ami           = data.aws_ami.ubuntu.id
-  instance_type = var.tipos_images[var.instance_type]
+  instance_type = var.instance_type
   key_name      = aws_key_pair.chave_ssh_desafio.key_name
   vpc_security_group_ids = [
     aws_security_group.libera_ssh.id
