85b9e9060289e56683562c771e13beb18ddad0d7
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2022-04-12 06:23:40 -0300

    criando vpc

---
+++ 8-vpc/1/README.md
@@ -0,0 +1,2 @@
+# tabela de calculo rede
+https://www.site24x7.com/pt/tools/ipv4-sub-rede-calculadora.html
---
+++ 8-vpc/1/subnets.tf
@@ -0,0 +1,29 @@
+resource "aws_subnet" "subnet_a1" {
+  vpc_id = aws_vpc.vpc_desafio.id
+  cidr_block = "10.0.64.0/18"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    "Name" = "subnet-a1"
+  }
+}
+
+resource "aws_subnet" "subnet_b1" {
+  vpc_id = aws_vpc.vpc_desafio.id
+  cidr_block = "10.0.128.0/18"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    "Name" = "subnet-b1"
+  }
+}
+
+resource "aws_subnet" "subnet_c1" {
+  vpc_id = aws_vpc.vpc_desafio.id
+  cidr_block = "10.0.192.0/18"
+  availability_zone = "us-east-1c"
+
+  tags = {
+    "Name" = "subnet-c1"
+  }
+}
