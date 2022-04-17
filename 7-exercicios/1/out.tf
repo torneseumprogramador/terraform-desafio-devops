cc12c0a7113123cea7082491eb319c41fcc98d5d
Danilo Santos <danilo.aparecido.santos@gmail.com>
2022-04-17 05:49:13 -0300

    versionando meu código

--- 7-exercicios/1/out.tf
+++ 7-exercicios/1/out.tf
@@ -1,6 +1,6 @@
 output "informacoes_vms" {
   value = [
     for vm in aws_instance.vm :
-    "ssh -i /Users/danilo/Desktop/desafio_devops/terraform/ssh/id_rsa ubuntu@${vm.public_dns} -o ServerAliveInterval=60 "
+    "ssh -i ../../ssh/id_rsa ubuntu@${vm.public_dns} -o ServerAliveInterval=60 "
   ]
 }
