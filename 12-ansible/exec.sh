91bbcbdae863b466be8175a2b9405d6fd0c88ec0
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2022-04-18 06:42:30 -0300

    um script legal juntando, shell, ansible, terraform

---
+++ 12-ansible/exec.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+cd terraform/
+terraform init
+terraform apply -auto-approve
+
+ANSIBLE_RETORNO=$(terraform output)
+DNS=(${ANSIBLE_RETORNO//=/ })
+DNS=${DNS[1]}
+DNS=$(echo $DNS | sed -e "s/\"//g")
+
+# DNS=$(terraform output public_dns) # Rafa mais reduzido
+
+cd ../1
+
+echo "
+[maquina-ansible]
+$DNS
+" > hosts
+
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key ../../ssh/id_rsa
+
+ssh -i ../../ssh/id_rsa ubuntu@$DNS -o ServerAliveInterval=60
